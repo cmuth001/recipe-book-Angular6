19aa8b8c2f914fdb9b667a1c307f48e7f750b504
cmuth001 <cmuth001@odu.edu>
2018-09-21 02:25:12 -0400

    Displaying recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test recipe','https://images-gmi-pmc.edge-generalmills.com/8a61d4b7-8967-47f0-b486-71a0e86ca82a.jpg' ),
+    new Recipe('A Test Recipe', 'This is simply a test recipe','https://images-gmi-pmc.edge-generalmills.com/8a61d4b7-8967-47f0-b486-71a0e86ca82a.jpg' ),
     new Recipe('A Test Recipe', 'This is simply a test recipe','https://images-gmi-pmc.edge-generalmills.com/8a61d4b7-8967-47f0-b486-71a0e86ca82a.jpg' )
   ];
   constructor() { }
