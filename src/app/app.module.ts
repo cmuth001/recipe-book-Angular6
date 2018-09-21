291126169c40974fcd5efa726a005855957c9daf
cmuth001 <cmuth001@odu.edu>
2018-09-21 00:54:17 -0400

    added required components to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ReipeListComponent } from './recipes/reipe-list/reipe-list.component';
+import { ReipeDetailComponent } from './recipes/reipe-detail/reipe-detail.component';
+import { RecipeItemComponent } from './recipes/reipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ReipeListComponent,
+    ReipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/reipe-list/reipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reipe-list',
+  templateUrl: './reipe-list.component.html',
+  styleUrls: ['./reipe-list.component.css']
+})
+export class ReipeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
